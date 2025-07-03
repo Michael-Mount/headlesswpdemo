616c47119e44b9d81c0920866b6f326b05d7df26
Michael Mount <michaeljoesphmount@gmail.com>
2025-07-03 09:47:21 -0600

    Removed 'link' tags from App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,6 @@ import Event from "./pages/Events.js";
 function App() {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/Events">Events</Link>
-          </li>
-        </ul>
-      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Events" element={<Event />} />
