636bef950c0c107662c49ea5f70686225dac68b4
Michael Mount <michaeljoesphmount@gmail.com>
2025-07-04 04:50:47 -0600

    Set up app.js with basic Route strucutre

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar.js";
+//Pages
 import Home from "./pages/Home";
 import Event from "./pages/Events.js";
+import Amenities from "./pages/Amenities";
+import Contact from "./pages/Contact";
+import Employment from "./pages/Employment";
+import Gather from "./pages/Gather";
+import HoneyGinger from "./pages/HoneyGinger";
+import OurStory from "./pages/OurStory";
+import Packages from "./pages/Packages";
+import RBar from "./pages/RBar";
+import RhylandRec from "./pages/RhylandRec";
+import Rooms from "./pages/Rooms";
+import Scene from "./pages/Scene";
+import Stay from "./pages/Stay";
 
 function App() {
   return (
@@ -9,7 +22,20 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Events" element={<Event />} />
+        <Route path="/events" element={<Event />} />
+        <Route path="/amenities" element={<Amenities />} />
+        <Route path="/contact-us" element={<Contact />} />
+        <Route path="/employment" element={<Employment />} />
+        <Route path="/gather" element={<Gather />} />
+        <Route path="/honey-ginger" element={<HoneyGinger />} />
+        <Route path="/our-story" element={<OurStory />} />
+        <Route path="/packages-specials" element={<Packages />} />
+        <Route path="/rbar" element={<RBar />} />
+        <Route path="/rhyland-recomandations" element={<RhylandRec />} />
+        <Route path="/rooms-suites" element={<Rooms />} />
+        <Route path="/the-scene" element={<Scene />} />
+        <Route path="/stay" element={<Stay />} />
+        <Route path="*" element={<div>Page Not Found</div>} />
       </Routes>
     </>
   );
