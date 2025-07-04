47482de4af5362bcf825a3d00283d7f303de2627
Michael Mount <michaeljoesphmount@gmail.com>
2025-07-04 04:10:15 -0600

    imoprting Navbar component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { Route, Routes, Link } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar.js";
 import Home from "./pages/Home";
 import Event from "./pages/Events.js";
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Events" element={<Event />} />
