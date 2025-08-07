00f9e4853eba7c8e3b97e99e2d43414b63c18c28
Michael Mount <michaeljoesphmount@gmail.com>
2025-08-07 12:11:58 -0600

    Testing Gsap animation on tall image gallery

--- src/components/features/TallImageGallery/TallImageGallery.js
+++ src/components/features/TallImageGallery/TallImageGallery.js
@@ -1,8 +1,34 @@
+import { useRef, useEffect } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import "./TallImageGallery.css";
 
+gsap.registerPlugin(ScrollTrigger);
+
 export default function TallImageGallery({ images }) {
+  const tallImageRef = useRef(null);
+  useEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap.from(tallImageRef.current, {
+        opacity: 0,
+        y: 50,
+        duration: 1,
+        ease: "power2.out",
+        scrollTrigger: {
+          trigger: tallImageRef.current,
+          start: "top 50%",
+          scrub: true,
+          markers: true,
+        },
+      });
+    });
+
+    return () => ctx.revert();
+  }, []);
+
   return (
-    <div className="tallImageContainer">
+    <div className="tallImageContainer" ref={tallImageRef}>
       {images.map((src, index) => (
         <div className="tallImageWrapper" key={index}>
           <img src={src} alt={`Gallery ${index + 1}`} />
